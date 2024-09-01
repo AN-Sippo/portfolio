9963f789c0170009c30175e28ae0b325eaf313d0
AN-Sippo <oppis02.anzu@gmail.com>
2024-09-01 18:04:34 +0900

    アイコンにリンクを仕込む機会が多そうなのでiconButton作ってみる。インターフェースについては実験的。onClickとかの方がいい気もする

---
+++ app/components/iconButtons/iconButtons.tsx
@@ -0,0 +1,22 @@
+import { LinksFunction } from "@remix-run/node";
+import {
+  Icons,
+  type Props as IconProps,
+  links as iconLinks,
+} from "../Icons/icons";
+
+export const links: LinksFunction = () => {
+  return [...iconLinks()];
+};
+
+type Props = IconProps & {
+  href?: string;
+};
+
+export const IconButton = ({ href, ...props }: Props) => {
+  return (
+    <a href={href}>
+      <Icons {...props} />
+    </a>
+  );
+};
