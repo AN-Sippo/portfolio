11a9d08a89e386933311820bd4b7682203895f87
AN-Sippo <oppis02.anzu@gmail.com>
2025-01-14 16:44:14 +0900

    パンくずリスト追加

---
+++ app/components/Breadcrumb/breadcrumb.tsx
@@ -0,0 +1,68 @@
+import { LinksFunction } from "@remix-run/cloudflare";
+import { Link, useLocation } from "@remix-run/react";
+import { Fragment } from "react/jsx-runtime";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: "public/assets/css/components/breadCrumbs.css",
+  },
+];
+
+type Route = {
+  label: string;
+  path: string;
+};
+
+const routes: Record<string, Route> = {
+  "/": {
+    label: "トップ",
+    path: "/",
+  },
+  "/blog": {
+    label: "ブログ一覧",
+    path: "/blog",
+  },
+};
+
+const detectRoutes = (path: string): Array<Route> => {
+  const segments = path.split("/").filter((value) => value != "");
+  let currentPath = "/";
+  const paths = [currentPath];
+  for (const segment of segments) {
+    currentPath = currentPath + segment;
+    paths.push(currentPath);
+    currentPath = currentPath + "/";
+  }
+  return paths.map((path) => routes[path]);
+};
+
+export const BreadCrumbs = () => {
+  const location = useLocation();
+  const routeArray = detectRoutes(location.pathname);
+
+  return (
+    <nav id="bread-crumbs-root">
+      {routeArray.map((route, index) => (
+        <Fragment key={route.path}>
+          {index !== 0 && <BreadCrumbsSeparator key={route.path + "-sep"} />}
+          <BreadCrumbsItem route={route} />
+        </Fragment>
+      ))}
+    </nav>
+  );
+};
+
+const BreadCrumbsItem = ({ route }: { route: Route }) => {
+  return (
+    <>
+      <Link to={route.path} className="bread-crumbs-item">
+        {route.label}
+      </Link>
+    </>
+  );
+};
+
+const BreadCrumbsSeparator = () => {
+  return <span className="bread-crumbs-separator">{">"}</span>;
+};
--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,10 +1,15 @@
 import { useLoaderData } from "@remix-run/react";
-import { Articles, Blog, links as blogLinks } from "~/components/Home/blog";
+import { Blog, links as blogLinks } from "~/components/Home/blog";
 import { loadRecentArticles } from "~/utils/blog/blog";
 import { json } from "@remix-run/react";
+import {
+  BreadCrumbs,
+  links as breadCrumbsLinks,
+} from "~/components/Breadcrumb/breadcrumb";
 
 export const links = () => [
   ...blogLinks(),
+  ...breadCrumbsLinks(),
   { rel: "stylesheet", href: "public/assets/css/routes/blog.css" },
 ];
 
@@ -17,6 +22,7 @@ export default function BlogPage() {
   const { articles } = useLoaderData<typeof loader>();
   return (
     <div id="route-container">
+      <BreadCrumbs />
       <h2>ブログ一覧</h2>
       <Blog articles={articles} articlesOnly />
     </div>
